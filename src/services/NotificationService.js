42fc6ec14ee84533cd084eb5ac0d11663185a13a
212778798 <chris1.higginbotham@ge.com>
2025-02-13 18:54:45 -0500

    update endpoint url

--- src/services/NotificationService.js
+++ src/services/NotificationService.js
@@ -5,13 +5,13 @@ const API_BASE_URL = 'https://74rk0lon11.execute-api.us-east-1.amazonaws.com/non
 export default {
   // Send notification through backend
   async sendNotification(message) {
-    const response = await axios.post(`${API_BASE_URL}/trigger-notifications`, { message });
+    const response = await axios.post(`${API_BASE_URL}/storeMessages`, { message });
     return response.data;
   },
 
   // Fetch list of notifications (optional if backed by the backend storage)
   async getNotifications() {
-    const response = await axios.get(`${API_BASE_URL}/Notifications`);
+    const response = await axios.get(`${API_BASE_URL}/fetchMessages`);
     return response.data;
   },
 };
