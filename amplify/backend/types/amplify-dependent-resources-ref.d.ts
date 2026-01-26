3e45a4a1949b0adebc9b47328f992fcfe6012b52
Chris <christopherhigginbotham@Christophers-MacBook-Pro.local>
2026-01-26 15:40:30 -0500

    update login screen and headers

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,16 @@
-export type AmplifyDependentResourcesAttributes = {}
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "notificationsui8680d471": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    },
+    "userPoolGroups": {
+      "notificationsGroupRole": "string"
+    }
+  }
+}
